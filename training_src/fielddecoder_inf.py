b25baec9aad068be70a1d46e2b524365a1d5c494
jiang719 <jiang719@purdue.edu>
2024-10-18 14:30:13 -0400

    fix model load path

--- training_src/fielddecoder_inf.py
+++ training_src/fielddecoder_inf.py
@@ -1,21 +1,20 @@
 import json
 import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
-from peft import PeftModel
 import argparse
 from huggingface_hub import login
 import os 
+
 hf_key = os.environ['HF_TOKEN']
 login(token = token)
 
 def inference(test_fpath, out_fpath, model_path):
     print('==========start loading model==========')
-    device_map = {'transformer.wte.weight': 0, 'transformer.wpe.weight': 0, 'transformer.ln_f': 0, 'lm_head': 0}
-    device_map.update({'transformer.h.' + str(i): 0 for i in range(0, 40)})
-    tokenizer = AutoTokenizer.from_pretrained('bigcode/starcoder', use_auth_token=hf_key)
+    
+    tokenizer = AutoTokenizer.from_pretrained('bigcode/starcoderbase-3b', use_auth_token=hf_key)
     model = AutoModelForCausalLM.from_pretrained(
         model_path, use_auth_token=hf_key,
-        torch_dtype=torch.bfloat16, device_map=device_map
+        torch_dtype=torch.bfloat16, device_map='auto'
     )
 
     wp = open(out_fpath, 'w')
@@ -25,10 +24,10 @@ def inference(test_fpath, out_fpath, model_path):
             line = json.loads(line)
             first_token = line['output'].split(':')[0]
             prompt = line['input'] + first_token + ':'
-            input_ids = tokenizer.encode(prompt, return_tensors='pt').cuda()
+            input_ids = tokenizer.encode(prompt, return_tensors='pt').cuda()[:, : 8192 - 1024]
             output = model.generate(
                 input_ids=input_ids, max_new_tokens=1024, num_beams=4, num_return_sequences=1, do_sample=False,
-                early_stopping=False, pad_token_id=0, eos_token_id=0
+                early_stopping=False, pad_token_id=tokenizer.eos_token_id, eos_token_id=tokenizer.eos_token_id
             )[0]
             output = tokenizer.decode(output[input_ids.size(1): ], skip_special_tokens=True, clean_up_tokenization_spaces=True)
             output = first_token + ':' + output
--- training_src/vardecoder_inf.py
+++ training_src/vardecoder_inf.py
@@ -10,12 +10,11 @@ hf_key = os.environ['HF_TOKEN']
 
 def inference(test_fpath, out_fpath, model_path):
     print('==========start loading model==========')
-    device_map = {'transformer.wte.weight': 0, 'transformer.wpe.weight': 0, 'transformer.ln_f': 0, 'lm_head': 0}
-    device_map.update({'transformer.h.' + str(i): 0 for i in range(0, 40)})
-    tokenizer = AutoTokenizer.from_pretrained('bigcode/starcoder', use_auth_token=hf_key)
+
+    tokenizer = AutoTokenizer.from_pretrained('bigcode/starcoderbase-3b', use_auth_token=hf_key)
     model = AutoModelForCausalLM.from_pretrained(
         model_path, use_auth_token=hf_key,
-        torch_dtype=torch.bfloat16, device_map=device_map
+        torch_dtype=torch.bfloat16, device_map='auto'
     )
 
     wp = open(out_fpath, 'w')
@@ -28,7 +27,7 @@ def inference(test_fpath, out_fpath, model_path):
 
             start_time = time.time()
 
-            input_ids = tokenizer.encode(prompt, return_tensors='pt').cuda()[:, : 8192 - 1024 - 2]
+            input_ids = tokenizer.encode(prompt, return_tensors='pt').cuda()[:, : 8192 - 1024]
             output = model.generate(
                 input_ids=input_ids, max_new_tokens=1024, num_beams=4, num_return_sequences=1, do_sample=False,
                 early_stopping=False, pad_token_id=0, eos_token_id=0
